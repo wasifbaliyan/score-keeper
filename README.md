4fd2b3224d67d35f7bdccbe0414f7bef8d1d8d78
Wasif Baliyan <w.baliyan@gmail.com>
2020-03-15 20:16:26 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Score Keeper
+
+** Built using HTML, CSS and Vanilla JS.**
---
+++ score.js
@@ -0,0 +1,44 @@
+let player1 = document.querySelector(".p1");
+let player2 = document.querySelector(".p2");
+let reset = document.querySelector(".reset");
+let score1 = document.querySelector(".one");
+let score2 = document.querySelector(".two");
+let inputScore = document.querySelector("input");
+let maxScore = document.querySelector(".scoreUpTo");
+let gameOver = false;
+let winningScore = 5;
+
+player1.addEventListener("click", function() {
+  if (!gameOver) {
+    score1.textContent++;
+    if (score1.textContent == winningScore) {
+      gameOver = true;
+      score1.classList.add("success");
+    }
+  }
+});
+player2.addEventListener("click", function() {
+  if (!gameOver) {
+    score2.textContent++;
+    if (score2.textContent == winningScore) {
+      gameOver = true;
+      score2.classList.add("success");
+    }
+  }
+});
+function resetGame() {
+  score1.textContent = 0;
+  score2.textContent = 0;
+  gameOver = false;
+  score1.classList.remove("success");
+  score2.classList.remove("success");
+}
+reset.addEventListener("click", function() {
+  resetGame();
+});
+
+inputScore.addEventListener("change", function() {
+  maxScore.textContent = this.value;
+  winningScore = Number(this.value);
+  resetGame();
+});
